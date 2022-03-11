237748cee48479414458c1d36ca763d612202566
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-03-11 16:12:01 -0300

    Initial commit

---
+++ src/main/java/com/viniciusgranado/senaipibackend/SenaiPiBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusgranado.senaipibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenaiPiBackEndApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SenaiPiBackEndApplication.class, args);
+  }
+
+}
