299ca99c313e30157b96620c4d2b1e3ba1776e7b
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-03-19 00:25:47 -0300

    Added insert user endpoint

--- src/main/java/com/viniciusgranado/senaipibackend/services/UserService.java
+++ src/main/java/com/viniciusgranado/senaipibackend/services/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
 
     return obj.orElseThrow(() -> new ResourceNotFoundException(id));
   }
+
+  public User insert(User obj) {
+    return repository.save(obj);
+  }
 }
